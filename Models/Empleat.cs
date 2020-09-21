c9c3a3ca4627e02000d59e9f24b12132823ddd2e
Jordi <jordi.contreras@gmail.com>
2020-09-21 17:50:09 +0200

    Get Hola mundo and Get Hola Name

---
+++ Models/Empleat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Empleats.Models
+{
+    public class Empleat
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public double Salary { get; set; }
+        public string Role { get; set; }
+    }
+}
